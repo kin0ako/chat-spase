e2ed7947d1218ebdcb78a8ef9793bb1b9134e767
kin0ako <email@example.com>
2019-12-10 21:29:23 +0900

    edit deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,7 +41,7 @@ set :linked_files, %w{ config/secrets.yml }
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
-    invokezw 'unicorn:restart'
+    invoke! 'unicorn:restart'
   end
 
   desc 'upload secrets.yml'
