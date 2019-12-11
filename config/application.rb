05fa537b9f83b4d8137ed2891cc66bca404e75be
kin0ako <email@example.com>
2019-12-11 18:27:12 +0900

    css

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,10 @@ module ChatSpase
       g.javascripts false
       g.helper false
       g.test_framework false
-      config.i18n.default_locale = :ja 
+      config.i18n.default_locale = :ja
     end
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
