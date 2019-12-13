7804de00b6fdcb51e3097b0ae1517204d0474c96
kin0ako <email@example.com>
2019-12-13 09:58:19 +0900

    change

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -59,7 +59,7 @@ $(function(){
           insertHTML = buildHTML(message);
           $('.messages').append(insertHTML);
         })
-      // $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'slowly');
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'slowly');
       })
       .fail(function() {
         alert('失敗しました');
